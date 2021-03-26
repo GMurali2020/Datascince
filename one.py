d7a04190166332c867d4f6dc67a782d78eba47a6
G murali <69345474+GMurali2020@users.noreply.github.com>
2021-03-26 11:40:31 +0530

    Update one.py

--- one.py
+++ one.py
@@ -5,3 +5,4 @@ print(a+b)
 hi how are you,how can i help you
 python is an open source gearnal purpose programing language
 it is an interpreted ,python supports multiple domines
+my name is hari i am working as a ai scientist at bell laboratory
