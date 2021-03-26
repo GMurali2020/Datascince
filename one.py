ca25dd21c8590c8c84df6980c1e8c1bbf57542a7
Muralidevoloper <69345474+Muralidevoloper@users.noreply.github.com>
2021-03-26 11:43:09 +0530

    secound commit

--- one.py
+++ one.py
@@ -6,3 +6,6 @@ hi how are you,how can i help you
 python is an open source gearnal purpose programing language
 it is an interpreted ,python supports multiple domines
 my name is hari i am working as a ai scientist at bell laboratory
+hari
+hari bhargav
+hrsha krish
